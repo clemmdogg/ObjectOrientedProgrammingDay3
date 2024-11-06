599e5a111554f247cadc9b1c43e2e5cf477c18fd
Casper Clemmensen <clemmensen.casper@gmail.com>
2024-11-06 10:29:36 +0100

    Minor changes

--- Program.cs
+++ Program.cs
@@ -9,13 +9,13 @@ namespace ObjectOrientedProgrammingDay3
             string carBrand = "Audi";
             string carModel = "A6";
             DateTime carProductionDate = new DateTime(2018, 7, 1);
-            DateTime carLastInspection = new DateTime(2022, 12, 8);
+            DateTime carLastInspection = new DateTime(2018, 12, 8);
             Vehicle car = new Car(carBrand, carModel, carProductionDate, carLastInspection);
 
             string truckBrand = "Volvo";
             string truckModel = "H6";
             DateTime truckProductionDate = new DateTime(2018, 10, 1);
-            DateTime truckLastInspection = new DateTime(2021, 10, 1);
+            DateTime truckLastInspection = new DateTime(2024, 10, 1);
             Vehicle truck = new Truck(truckBrand, truckModel, truckProductionDate, truckLastInspection);
 
             car.DisplayInfo();
